94e21503c777952614662a5bb6a7693597f9eec0
Modulus <thejohnskauge@gmail.com>
2015-03-23 16:21:01 +0100

    Added warning alert, if text is emtpy

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,15 +12,26 @@ angular.module('wailerGuiApp')
     this.$inject = ['$scope', '$log', 'nameService', 'wailService']
     $scope.newWail = "";
     $scope.wails = [];
+    $scope.error = ""
 
-    $scope.pushWail = function(){
-        $log.info('Pubhsing new wail to wails: '+$scope.newWail);
+    $scope.pushWail = function () {
+      if ($scope.newWail) {
+        $scope.error = "";
+        $log.info('Pubhsing new wail to wails: ' + $scope.newWail);
         var wail = {text: $scope.newWail, name: nameService.createName(), timestamp: new Date()}
         wailService.postWail(wail);
+        $log.info('Clearing new wail');
+        $scope.newWail = "";
+      }
+      else {
+          $log.error("No text entered, showing error dialog.");
+          $scope.error = "Please give us text, GIVE US TEXT!!! Pretty please.";
+      }
     };
 
-    function init(){
+    function init() {
       $scope.wails = wailService.getWails();
     }
+
     init();
   });
