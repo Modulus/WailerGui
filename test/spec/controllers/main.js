a509ac6c1e26109b37efc42bc98dd3eab3bebd88
Modulus <thejohnskauge@gmail.com>
2015-03-23 16:48:13 +0100

    Created mock for test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,8 +2,24 @@
 
 describe('Controller: MainCtrl', function () {
 
+  var wails = [{text: 'I hate haters', name: 'Scarce chuckwalla', timestamp: new Date() },
+    {text: 'I\'m sick of all this here nonsense guys!', name: 'Clever boutu', timestamp: new Date() },
+    {text: 'Why am I here?', name: 'Roy', timestamp: new Date()},
+    {text: 'HULK SMASH!!!!', name: 'The Hulk', timestamp: new Date()}];
+
   // load the controller's module
-  beforeEach(module('wailerGuiApp'));
+  beforeEach(module('wailerGuiApp', function($provide){
+    var WailService = {
+
+
+        getWails: function () {
+          return wails
+        },
+        postWail: function(wail){
+          wails.push(wail);
+        }
+      };
+  }));
 
   var MainCtrl,
     scope;
@@ -19,6 +35,7 @@ describe('Controller: MainCtrl', function () {
   it('should have these fields declared', function(){
     expect(scope.newWail).toBeDefined();
     expect(scope.wails).toBeDefined();
+    expect(scope.error).toBeDefined();
   });
 
   it('should have a newWail with an empty string', function () {
@@ -28,5 +45,21 @@ describe('Controller: MainCtrl', function () {
 
   it('should have an empty list of wails', function(){
     expect(scope.wails.length).toBeGreaterThan(1);
+  });
+
+  it('should have an emtpy error message at the beginning', function(){
+    expect(scope.error.length).toBe(0);
+  });
+
+  it('should have an error message if empty wail was tried to be inserted', function(){
+    scope.newWail = "";
+    scope.pushWail();
+    expect(scope.error.length).toBeGreaterThan(0)
+  });
+
+  it('should not have an error message if newWail has text', function(){
+    scope.newWail = {text: 'bleuh bleush blaaaargh!', name: 'Nefarious Guinea Turaco', timestamp: new Date() }
+    scope.pushWail();
+    expect(scope.error.length).toBe(0);
   });
 });
